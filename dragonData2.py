69b580385195538fd0957f0cc46fa03ab179a027
EMBTonkin <LizzieTonkin@gmail.com>
2015-10-08 22:01:34 -0400

    Update to the official DRG format.
    The program now reads and writes files in the DRG format.
    The readFromFile function and the saveInFile functions were changes,
    with a couple of new fields and parameters added to the Dragon and Data objects.

--- dragonData2.py
+++ dragonData2.py
@@ -1,5 +1,5 @@
 # Lizzie Tonkin
-# Last edited: Oct 3, 2015
+# Last edited: Oct 8, 2015
 # Based on:
 #	CS 251 2014
 #	Project 5
@@ -9,7 +9,7 @@
 
 import displayData as d
 import string
-
+import xml.etree.ElementTree as ET
 
 
 """
@@ -28,7 +28,8 @@ string list 3 genes
 string notes no commas please
 """
 
-
+## need to fix the tree issues, and make the tree more intigrated
+## tree issues is that if the dragons in not created in this program, things will go weird
 
 """
 Ancestry map (not drawn to implementation)
@@ -58,7 +59,7 @@ children
 # see above for format
 # no special accessors or mutators yet, but there will be some cool ones soon!
 class Dragon:
-	def __init__(self, id, name, gen, ansestors, decendants, exalt, matingType, species, colors, genes, notes):
+	def __init__(self, id, name, gen, ansestors, decendants, exalt, matingType, species, colors, genes, notes, treeNode=None):
 		self.id = id
 		self.name = name
 		self.gen = gen
@@ -74,6 +75,8 @@ class Dragon:
 		self.colors = colors
 		self.genes = genes
 		self.notes = string.replace(notes , "\\n" , "\n")  # fixes bug where where newlines display as literal '\n'  See below for reason.
+		
+		self.treeNode = treeNode
 		
 		# display stuff that is filled in later
 		self.rect = None
@@ -125,12 +128,14 @@ class Data:
 
 	#creates a Data object based on a filename.	 The file must be in .csv format.
 	def __init__(self, filename = None, enums = False, data= None):
+		self.DRG= None
 		self.IDmap = {}
 		self.genMap = {}
 		if filename == None:
 			self.dragonList = []
 			return
 		self.dragonList = self.readFromFile(filename)
+		
 		
 		# attach parents and add all dragons to ALL the lists/dictionaries
 		parented = []
@@ -252,85 +257,174 @@ class Data:
 		#self.genMap[dragon.gen].remove(dragon)
 		
 	
-	#takes a filename, which must be in .csv format, and reads it in to creat a 2D array of the semi-raw data
-	# I say semi because the numbers will be converted to actual numbers, not just strings of numbers. #####
+	#takes a filename, which must be in DRG format, and reads it in to creat a list
 	# The data is then put into a Dragon object, for slightly better OO design
+	# will need to re-do for to make use of the XML tree for even BETTER OO design
 	def readFromFile(self, filename):
-		fobj = file(filename)
-		lines = fobj.readlines()
-		fobj.close()
-		words = []
+		self.DRG = ET.parse(filename)
+		mainFamily = self.DRG.find('Family')
+		allDragons = mainFamily.findall('Dragon')
+		
+		# list of dragons we will return
 		table = []
-		if len(lines) == 1:
-			lines = lines[0].split("\r")
-		for line in lines:
-			words = line.strip().split(",")
+		
+		# all the dragon tags
+		for aDragon in allDragons:
+		
+			if aDragon.find('DAL9000') == None:
+				print 'I am a terrible person and don\'t actually support dragons created in other programs yet.  Sorry.'
+				quit()
 			
-			# I was horribly lazy and parsed each line individually, instead of making an automated process.  Whatever. #####
-			words[0]= int(words[0])
-			words[1]= words[1].strip()
-			words[2]= int(words[2])
-			
-			# unpacking stringified 2D arrays is hard
-			parsed = words[3].strip(' [').replace(']','').split('[')
-			#print parsed
-			assembled = []
-			for gen in parsed:
-				assembled+= [gen.strip().split(' ')]
-			words[3]= [[],[],[],[],[]]
-			for i in range(5):
-				for j in range(len(assembled[i])):
-					if assembled[i][j] != '':
-						words[3][i].append( int(assembled[i][j]))
-			
-			parsed = words[4].strip(' [').replace(']','').split('[')
-			assembled = []
-			for gen in parsed:
-				assembled+= [gen.strip().split(' ')]
-			words[4]= [[],[],[],[],[]]
-			for i in range(5):
-				for j in range(len(assembled[i])):
-					if assembled[i][j] != '':
-						words[4][i].append( int(assembled[i][j]))
-			
-			if words[5].strip() == "False":
-				words[5] = False
+			id = int(aDragon.find('id').text)
+			name = aDragon.find('name').text
+			value = aDragon.find('exalted').text
+			if value == 'True':
+				exalt = True
 			else:
-				words[5]= True
-			
-			if words[6].strip() == "False":
-				words[6] = False
+				exalt = False
+			value = aDragon.find('matingType').text
+			if value == 'True':
+				matingType = True
 			else:
-				words[6]= True
+				matingType = False
+			species = aDragon.find('species').text
+			primary = aDragon.find('primary').text.split(' ')
+			secondary = aDragon.find('secondary').text.split(' ')
+			tertiary = aDragon.find('tertiary').text.split(' ')
+			genes = [primary[1], secondary[1], tertiary[1]]
+			colors = [primary[0], secondary[0], tertiary[0]]
+			notes = aDragon.find('comment').text
 			
-			words[7]= words[7].strip()
+			##############
+			# stuff that will hav to change
+			myTag = aDragon.find('DAL9000')
+			gen = int(myTag.find('gen').text)
 			
-			words[8]= words[8].strip().split(" ")
+			ansesestors1 = aDragon.find('parents').text
+			ansesestors2 = myTag.find('anestors2').text
+			ansesestors3 = myTag.find('anestors3').text
+			ansesestors4 = myTag.find('anestors4').text
+			ansesestors5 = myTag.find('anestors5').text
+			ansestors = [ansesestors1,ansesestors2,ansesestors3,ansesestors4,ansesestors5]
+			
+			decendants1 = aDragon.find('parents').text
+			decendants2 = myTag.find('decendants2').text
+			decendants3 = myTag.find('decendants3').text
+			decendants4 = myTag.find('decendants4').text
+			decendants5 = myTag.find('decendants5').text
+			decendants = [decendants1,decendants2,decendants3,decendants4,decendants5]
+			
+			# need to make Nones into empty lists, and convert strings to ints
+			for i in range(4,-1,-1):
+				if decendants[i] != None:
+					decendants[i]=decendants[i].split(' ')
+					decendants[i] = [ int(x) for x in decendants[i] ]
+				else: 
+					decendants[i] = []
 				
-			words[9]= words[9].strip().split(" ")
-			words[10]= words[10].strip()
+				if ansestors[i] != None:
+					ansestors[i]=ansestors[i].split(' ')
+					ansestors[i] = [ int(x) for x in ansestors[i] ]
+				else: 
+					ansestors[i] = []
 			
-			table.append(Dragon(words[0],words[1],words[2],words[3],words[4],words[5],words[6],words[7],words[8],words[9],words[10]))			
+			# make a dragon and add it to the table	
+			table.append(Dragon(id,name,gen,ansestors,decendants,exalt,matingType,species,colors,genes,notes,aDragon))
 		
+		# don't forget to return the table!
 		return table
-	
-	# saves the current dragons in a fancy file, that can be reopened!	
-	def saveInFile(self,filename):
-		output = ""
 		
-		for j in range(len(self.genMap.keys())):
-			for dragon in self.genMap[j+1]:
-				output += dragon.saveFormat() + "\n"
-		f = open(filename, 'w')
-		print output
-		f.write(output)
-		f.close
-
-
-
-
-
-
+		
+	# saves the current dragons in a fancy file, that can be reopened!	
+	# Now in DRG format! (Or as close as I can get to my own impossible standards)
+	def saveInFile(self,filename):
+		
+		# if this is a new file, make the base enclosing tags and Element Tree
+		# will need to revisit once something happens with families
+		if self.DRG == None:
+			root = ET.Element('drg')
+			family = ET.SubElement(root, 'Family')
+			title = ET.SubElement(family, 'title')
+			title.text = "A Family"
+			self.DRG = ET.ElementTree(element=root)
+			
+		# going through all the dragons
+		for dragon in self.dragonList:
+			# if the dragon does not have a node, make one, and fill it with sub nodes
+			if dragon.treeNode == None:
+				mainFamily = self.DRG.find('Family')
+				print mainFamily
+				new = ET.SubElement(mainFamily, 'Dragon')
+				print new
+				dragon.treeNode = new
+				
+				ET.SubElement(new, 'id')
+				dragon.treeNode.find('id').text = str(dragon.id) # add the unchangeable
+				ET.SubElement(new, 'name')
+				
+				ET.SubElement(new, 'parents')
+				ET.SubElement(new, 'children')
+				
+				ET.SubElement(new, 'exalted')
+				ET.SubElement(new, 'matingType')
+				dragon.treeNode.find('matingType').text = str(dragon.matingType) # add the unchangeable
+				ET.SubElement(new, 'species')
+				
+				ET.SubElement(new, 'primary')
+				ET.SubElement(new, 'secondary')
+				ET.SubElement(new, 'tertiary')
+				ET.SubElement(new, 'comment')
+				
+				myTag = ET.SubElement(new, 'DAL9000')
+				ET.SubElement(myTag, 'gen')
+				ET.SubElement(myTag, 'anestors2')
+				ET.SubElement(myTag, 'anestors3')
+				ET.SubElement(myTag, 'anestors4')
+				ET.SubElement(myTag, 'anestors5')
+				ET.SubElement(myTag, 'decendants2')
+				ET.SubElement(myTag, 'decendants3')
+				ET.SubElement(myTag, 'decendants4')
+				ET.SubElement(myTag, 'decendants5')
+				
+				
+				
+				
+			# make sure stuff in node is up to date
+			dragon.treeNode.find('name').text = dragon.name
+			dragon.treeNode.find('exalted').text = str(dragon.exalt)
+			dragon.treeNode.find('species').text = dragon.species
+			dragon.treeNode.find('primary').text = dragon.colors[0]+" "+dragon.genes[0]
+			dragon.treeNode.find('secondary').text = dragon.colors[1]+" "+dragon.genes[1]
+			dragon.treeNode.find('tertiary').text = dragon.colors[2]+" "+dragon.genes[2]
+			dragon.treeNode.find('comment').text = dragon.notes
+			
+			myTag = dragon.treeNode.find('DAL9000')
+			myTag.find('gen').text = str(dragon.gen)
+			
+			# need to re-convert to strings.  What a pain
+			for i in range(5):
+				dragon.ansestors[i] = [ str(x) for x in dragon.ansestors[i] ]
+				dragon.decendants[i] = [ str(x) for x in dragon.decendants[i] ]
+				
+			dragon.treeNode.find('parents').text = ' '.join(dragon.ansestors[0])
+			myTag.find('anestors2').text = ' '.join(dragon.ansestors[1])
+			myTag.find('anestors3').text = ' '.join(dragon.ansestors[2])
+			myTag.find('anestors4').text = ' '.join(dragon.ansestors[3])
+			myTag.find('anestors5').text = ' '.join(dragon.ansestors[4])
+			
+			dragon.treeNode.find('children').text = ' '.join(dragon.decendants[0])
+			myTag.find('decendants2').text = ' '.join(dragon.decendants[1])
+			myTag.find('decendants3').text = ' '.join(dragon.decendants[2])
+			myTag.find('decendants4').text = ' '.join(dragon.decendants[3])
+			myTag.find('decendants5').text = ' '.join(dragon.decendants[4])
+				
+		self.DRG.write(filename, method = 'html')
+		
+		# to add the first line.  I like 'em
+		with file(filename, 'r') as original: data = original.read()
+		with file(filename, 'w') as modified: modified.write("<?xml version='1.0' encoding='UTF-8'?> \n" + data)		
+		
+		
 
 
 	
